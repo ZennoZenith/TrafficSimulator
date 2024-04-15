1e43499bd618861df242786700e6430277597c0a
ZennoZenith <mbali97655@gmail.com>
2024-04-15 18:04:28 +0530

    feat: Road mesh renderer implemented

--- Assets/_Scripts/Splines/RoadMeshGenerator.cs
+++ Assets/_Scripts/Splines/RoadMeshGenerator.cs
@@ -1,55 +1,103 @@
-using Unity.Mathematics;
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.Splines;
 
 
-[RequireComponent(typeof(SplineContainer))]
+[RequireComponent(typeof(SplineSampler))]
 public class RoadMeshGenerator : MonoBehaviour {
-    [SerializeField] private SplineContainer spline;
+    [SerializeField] private SplineSampler splineSampler;
 
     [SerializeField]
     private RoadMeshType roadMeshType;
 
-    [SerializeField] private int splineIndex;
+    [SerializeField]
+    [UnityEngine.Range(1, 200)]
+    private float resolution;
 
     [SerializeField]
-    [Range(0f, 1f)]
-    private float time;
-
-    [SerializeField]
-    [Range(0f, 10f)]
+    [UnityEngine.Range(0f, 10f)]
     private float width;
 
-    float3 position;
-    float3 forwardTangent;
-    float3 upVector;
+    List<Vector3> vertsP1;
+    List<Vector3> vertsP2;
 
     private void Update() {
-        SetPositionTangentUpvector();
+        SetVerts();
+        BuildMesh();
     }
 
     private void Start() {
-        spline = spline != null ? spline : GetComponent<SplineContainer>();
-    }
-
-    private void SetPositionTangentUpvector() {
-        spline.Evaluate(splineIndex, time, out position, out forwardTangent, out upVector);
+        splineSampler = GetComponent<SplineSampler>();
     }
 
     private void OnDrawGizmos() {
-        SetPositionTangentUpvector();
-        //Handles.matrix = transform.localToWorldMatrix;
-        //Handles.SphereHandleCap(0, position, Quaternion.identity, 1f, EventType.Repaint);
+        if (!Application.isPlaying)
+            return;
 
-        float3 right = Vector3.Cross(forwardTangent, upVector).normalized;
-        float3 p1 = position + (right * width);
-        float3 p2 = position + (-right * width);
-        Handles.SphereHandleCap(0, p1, Quaternion.identity, 1f, EventType.Repaint);
-        Handles.SphereHandleCap(0, p2, Quaternion.identity, 1f, EventType.Repaint);
+        for (int i = 0; i < vertsP1.Count; i++) {
+            Handles.SphereHandleCap(0, vertsP1[i], Quaternion.identity, 0.5f, EventType.Repaint);
+            Handles.SphereHandleCap(0, vertsP2[i], Quaternion.identity, 0.5f, EventType.Repaint);
+            Handles.DrawLine(vertsP1[i], vertsP2[i]);
+        }
+
     }
 
+    private void SetVerts() {
+        vertsP1 = new List<Vector3>();
+        vertsP2 = new List<Vector3>();
 
+        float step = 1f / (float)resolution;
+        for (int i = 0; i < resolution; i++) {
+            float t = step * i;
+            splineSampler.SampleSplineWidth(t, width, out Vector3 p1, out Vector3 p2);
+            vertsP1.Add(p1);
+            vertsP2.Add(p2);
+        }
 
+    }
+
+    private void BuildMesh() {
+        //MeshFilter meshFilter = gameObject.AddComponent<MeshFilter>();
+        MeshFilter meshFilter = gameObject.GetComponent<MeshFilter>();
+        Mesh m = new();
+        //Mesh m = gameObject.AddComponent<MeshFilter>().mesh;
+        List<Vector3> verts = new();
+        List<int> tris = new();
+        int numberOfVerts = vertsP1.Count;
+
+        for (int i = 1; i <= numberOfVerts; i++) {
+            Vector3 p1 = vertsP1[i - 1];
+            Vector3 p2 = vertsP2[i - 1];
+            Vector3 p3;
+            Vector3 p4;
+
+            if (i == numberOfVerts) {
+                p3 = vertsP1[0];
+                p4 = vertsP2[0];
+            }
+            else {
+                p3 = vertsP1[i];
+                p4 = vertsP2[i];
+            }
+
+            int offset = 4 * (i - 1);
+            int t1 = offset + 0;
+            int t2 = offset + 2;
+            int t3 = offset + 3;
+
+            int t4 = offset + 3;
+            int t5 = offset + 1;
+            int t6 = offset + 0;
+
+            verts.AddRange(new List<Vector3>() { p1, p2, p3, p4 });
+            tris.AddRange(new List<int>() { t1, t2, t3, t4, t5, t6 });
+
+        }
+
+        m.SetVertices(verts);
+        m.SetTriangles(tris, 0);
+
+        meshFilter.mesh = m;
+    }
 
 }
--- Assets/_Scripts/Splines/SplineSampler.cs
+++ Assets/_Scripts/Splines/SplineSampler.cs
@@ -1,54 +1,41 @@
 using Unity.Mathematics;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.Splines;
 
 [RequireComponent(typeof(SplineContainer))]
 public class SplineSampler : MonoBehaviour {
-    [SerializeField] private SplineContainer[] splines;
+    private SplineContainer[] splines;
 
     [SerializeField] private int splineIndex;
 
-    [SerializeField]
-    [Range(0f, 1f)]
-    private float time;
+    //[SerializeField]
+    //[Range(0f, 1f)]
+    //private float time;
 
-    [SerializeField]
-    [Range(0f, 10f)]
-    private float width;
 
     float3 position;
     float3 forwardTangent;
     float3 upVector;
 
     private void Start() {
-        if (splines.Length > 0) {
-            return;
-        }
+        //if (splines?.Length > 0) {
+        //    return;
+        //}
         splines = GetComponents<SplineContainer>();
 
     }
 
-    private void Update() {
-        //SetPositionTangentUpvector();
-    }
+    //private void SetPositionTangentUpvector() {
+    //    foreach (var spline in splines) {
+    //        spline.Evaluate(splineIndex, time, out position, out forwardTangent, out upVector);
+    //    }
+    //}
 
-    private void SetPositionTangentUpvector() {
-        foreach (var spline in splines) {
-            spline.Evaluate(splineIndex, time, out position, out forwardTangent, out upVector);
-        }
-    }
-
-    private void OnDrawGizmos() {
-        SetPositionTangentUpvector();
-        //Handles.matrix = transform.localToWorldMatrix;
-        //Handles.SphereHandleCap(0, position, Quaternion.identity, 1f, EventType.Repaint);
-
+    public void SampleSplineWidth(float time, float width, out Vector3 p1, out Vector3 p2) {
+        splines[splineIndex].Evaluate(splineIndex, time, out position, out forwardTangent, out upVector);
         float3 right = Vector3.Cross(forwardTangent, upVector).normalized;
-        float3 p1 = position + (right * width);
-        float3 p2 = position + (-right * width);
-        Handles.SphereHandleCap(0, p1, Quaternion.identity, 1f, EventType.Repaint);
-        Handles.SphereHandleCap(0, p2, Quaternion.identity, 1f, EventType.Repaint);
+        p1 = position + (right * width);
+        p2 = position + (-right * width);
     }
 
 }
